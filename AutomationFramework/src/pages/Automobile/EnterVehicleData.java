a45722c4a86e073d3d65513ac42de51ba886f348
santosh9505 <santoshkarthik.velamakanni@gmail.com>
2021-03-30 00:31:09 +0530

    commiting30/03/2021

--- AutomationFramework/src/pages/Automobile/EnterVehicleData.java
+++ AutomationFramework/src/pages/Automobile/EnterVehicleData.java
@@ -2,6 +2,8 @@ package pages.Automobile;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import functionLibrary.ApplicationLibrary;
 
@@ -18,10 +20,13 @@ public class EnterVehicleData {
 	//WebElements
 	By make=By.id("make");
 	By enginePerformance=By.id("engineperformance");
-	/*
-	 * You need to add more elements
-	 * 
-	 */
+	By dateofmanufacture = By.id("dateofmanufacture");
+	By numberofseats = By.id("numberofseats");
+	By fuel = By.id("fuel");
+	By listprice = By.id("listprice");
+	By licenseplatenumber = By.id("licenseplatenumber");
+	By annualmileage = By.id("annualmileage");
+	By nextenterinsurantdata = By.id("nextenterinsurantdata");
 	
 	//Visibility
 	public boolean isEnginePerformancePresent()
@@ -35,10 +40,87 @@ public class EnterVehicleData {
 		return lib.isElementPresent(dr, make);
 	}
 	
+	public boolean isDateOfManufacturePresent()
+	{
+		return lib.isElementPresent(dr,dateofmanufacture);
+	}
+	
+	public boolean isNumberOfSeats()
+	{
+		return lib.isElementPresent(dr,numberofseats);
+	}
+	
+	public boolean isFuel()
+	{
+		return lib.isElementPresent(dr,fuel);
+	}
+	
+	public boolean isListPrice()
+	{
+		return lib.isElementPresent(dr,listprice);
+	}
+	
+	public boolean isLicensePlateNumber()
+	{
+		return lib.isElementPresent(dr,licenseplatenumber);
+	}
+	public boolean isAnnualMileage()
+	{
+		return lib.isElementPresent(dr,annualmileage);
+	}
+	public boolean isNextEnterInsurantData()
+	{
+		return lib.isElementPresent(dr,nextenterinsurantdata);
+	}
+	
 	//Actions
 	public void enterEnginePerformance(int data)
 	{	
 		dr.findElement(enginePerformance).sendKeys(""+data);
 	}
 	
+	public void enterMake(int data1)
+	{
+		//WebElement make1 = dr.findElement(By.id("make"));
+		Select sel = new Select(dr.findElement(make));
+		sel.selectByIndex(data1);	
+	}
+	
+	public void enterDateOfManufacture(String data2 )
+	{
+		dr.findElement(dateofmanufacture).sendKeys(""+data2);
+	}
+	
+	public void enterNumberOfSeats(int data3)
+	{
+		Select sel = new Select(dr.findElement(numberofseats));
+		sel.selectByIndex(data3);	
+	}
+	
+	public void enterFuel(int data4)
+	{
+		Select sel = new Select(dr.findElement(fuel));
+		sel.selectByIndex(data4);
+	}
+	
+	public void enterListPrice(int data5)
+	{
+		dr.findElement(listprice).sendKeys(""+data5);
+	}
+	
+	public void enterLicensePlateNumber(int data6)
+	{
+		dr.findElement(licenseplatenumber).sendKeys(""+data6);
+	}
+	
+	public void enterAnnualMileage(int data7)
+	{
+		dr.findElement(annualmileage).sendKeys(""+data7);
+	}
+	
+	public void enterNextEnterInsurantData()
+	{
+		dr.findElement(nextenterinsurantdata).click();
+	}
 }
+
--- AutomationFramework/src/pages/Truck/SendQuote.java
+++ AutomationFramework/src/pages/Truck/SendQuote.java
@@ -1,5 +1,103 @@
 package pages.Truck;
 
-public class SendQuote {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+import functionLibrary.ApplicationLibrary;
+
+public class SendQuote {
+	
+	WebDriver dr;
+	ApplicationLibrary lib= new ApplicationLibrary();
+	
+	public SendQuote(WebDriver dr)
+	{
+		this.dr=dr;
+	}
+	
+	//WebElements
+	By email=By.id("email");
+	By phone=By.id("Phone");
+	By username = By.id("username");
+	By password = By.id("password");
+	By confirmpassword = By.id("confirmpassword");
+    By comments = By.id("Comments");
+    By sendemail = By.id("sendemail");
+    
+    public boolean isemail()
+	{
+		return lib.isElementPresent(dr,email);
+	}
+    
+    public boolean isphone()
+	{
+		return lib.isElementPresent(dr,phone);
+	}
+    
+    public boolean isusername()
+   	{
+   		return lib.isElementPresent(dr,username);
+   	}
+    
+    public boolean ispassword()
+   	{
+   		return lib.isElementPresent(dr,password);
+   	}
+    
+    public boolean isconfirmpassword()
+    {
+    	return lib.isElementPresent(dr, confirmpassword);
+    }
+    
+    public boolean iscomments()
+    {
+    	return lib.isElementPresent(dr, comments);
+    }
+    
+    public boolean isSendeMail()
+    {
+    	return lib.isElementPresent(dr, sendemail);
+    }
+    
+    public void entereMail(String data)
+	{	
+		dr.findElement(email).sendKeys(""+data);
+	}
+    
+    public void enterPhone(int data1)
+   	{	
+   		dr.findElement(phone).sendKeys(""+data1);
+   	}
+    
+    public void enterUsername(String data2)
+   	{	
+   		dr.findElement(username).sendKeys(""+data2);
+   	}
+    
+    public void enterPassword(String data3)
+   	{	
+   		dr.findElement(password).sendKeys(""+data3);
+   	}
+    
+    public void enterConfirmPassword(String data4)
+   	{	
+   		dr.findElement(confirmpassword).sendKeys(""+data4);
+   	}
+    
+    public void enterComments(String data5)
+   	{	
+   		dr.findElement(comments).sendKeys(""+data5);
+   	}
+    
+    public void clickSendEMail()
+	{	
+		dr.findElement(sendemail).click();
+	}
+    
+
+    
+    
+    
+    
+    
 }
